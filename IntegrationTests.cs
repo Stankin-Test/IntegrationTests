cfa3cbd3df6870c75bd61c42ece47237278f09f4
Vasiliy Kolenkin <v.kolenkin@stankin.ru>
2022-04-17 01:24:25 +0300

    Initial commit

---
+++ IntegrationTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class IntegrationTests
+{
+    [Test]
+    public async Task Test()
+    {
+        var httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("http://host.docker.internal:5043"),
+        };
+        var response = await httpClient.GetAsync("/WeatherForecast");
+        var content = await response.Content.ReadAsStringAsync();
+        TestContext.Out.WriteLine(content);
+        StringAssert.Contains("temperatureC", content);
+    }
+}
